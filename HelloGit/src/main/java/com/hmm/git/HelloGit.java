0b95ff3be63c77377148c2935ed401675e3ec9c9
MING-FINE <2491361136@qq.com>
2022-06-02 11:05:25 +0800

    第一次添加文件

---
+++ HelloGit/src/main/java/com/hmm/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.hmm.git;
+
+/*
+ * @author MING
+ * @date 2022/6/2
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
